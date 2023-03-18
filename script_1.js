492e86091193868d3966b4fee6b200c4e7c920cf
Kirill <kirillrocker@gmail.com>
2023-03-18 22:42:06 +0200

    func_v2

--- script_1.js
+++ script_1.js
@@ -1,13 +1,26 @@
-let str = 'sadasd12.32asdas';
-
 arr = [];
 
-for (let i=0; i<str.length; i++) {
-    if (str[i] >= 0 || str[i] == '.') {
-        arr.push(str[i]);
+function search (inp) {
+    for (let i=0; i<inp.length; i++) {
+      if (inp[i] >= 0) {
+        arr.push(inp[i]);
+      }
     }
+    return +arr.join('');
 }
-let out = +arr.join('');
 
-console.log( out);
-   
+console.log(search('asd123asd'))
+
+
+// console.assert(search('1234567890') === 1234567890, 1);
+// console.assert(search('') === null, 2);
+// console.assert(search('123asd') === 123, 3);
+// console.assert(search('asd123') === 123, 4);
+// console.assert(search('asd123asd') === 123, 5);
+// console.assert(search('asd123asd 123') === 123123, 6);
+// console.assert(search('123.1') === 123.1, 7);
+// console.assert(search('as123.1asd') === 123.1, 8);
+// console.assert(search('as123asd.1asd') === 123.1, 9);
+// console.assert(search('as123.asd1asd') === 123.1, 10);
+// console.assert(search('as.asdasd') === null, 11);
+// console.assert(search(123) === 123, 11);
